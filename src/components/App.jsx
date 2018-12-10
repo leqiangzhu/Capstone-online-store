d295b728cb2b0e0e5906211ecf3f4803d1ed0a31
David Zhu <zhuleqiang@gmail.com>
2018-12-09 20:23:21 -0800

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+//import PropTypes from 'prop-types';
+import { Switch, Route } from 'react-router-dom';
+
+/*
+  import { Link } from 'react-router-dom';
+  <Link to="/">Home</Link> | <Link to="/newticket">Create Ticket</Link>
+*/
+
+function App(){
+  var styles = {
+  };
+  return (
+    <div style={styles}>
+      <style jsx>{`
+        font-family: Helvetica;
+      `}</style>
+      CapsoneProject
+      {/* <Switch>
+        <Route exact path='/' component={} />
+      </Switch> */}
+    </div>
+  );
+}
+
+//App.propTypes = {
+//};
+
+export default App;
